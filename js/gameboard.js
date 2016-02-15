95c8d6c006b203a038a4749e5b77ca6637f6a934
Jayant Gandhi <jayant.gandhi@gmail.com>
2016-02-15 12:17:36 -0500

    draw player method

--- js/gameboard.js
+++ js/gameboard.js
@@ -1,32 +1,51 @@
 /**
- * Gameboard.js
- * Creates the Gameboard prototype
- */
+* Gameboard.js
+* Creates the Gameboard prototype
+*/
 
- function Gameboard(id, rows, cols, players) {
-   var id   = typeof id !== 'undefined' ? id : 'gameboard',
-       rows = typeof rows !== 'undefined' ? rows : 5,
-       cols = typeof cols !== 'undefined' ? cols : 5,
-       players = typeof players !== 'undefined' ? players : ['player1', 'player2', 'player3', 'player4'];
-   this.board = document.getElementById(id);
-   this.ctx = this.board.getContext('2d');
-   this.cellHeight = this.board.height / rows;
-   this.cellWidth = this.board.width /cols;
-   this.players = players;
+function Gameboard(id, rows, cols, players) {
+ var id   = typeof id !== 'undefined' ? id : 'gameboard',
+     rows = typeof rows !== 'undefined' ? rows : 5,
+     cols = typeof cols !== 'undefined' ? cols : 5,
+     players = typeof players !== 'undefined' ? players : ['player1', 'player2', 'player3', 'player4'];
+ this.board = document.getElementById(id);
+ this.ctx = this.board.getContext('2d');
+ this.cellHeight = this.board.height / rows;
+ this.cellWidth = this.board.width /cols;
+ this.players = players;
+}
+
+Gameboard.prototype.drawBoard = function () {
+ this.ctx.beginPath();
+
+ for (var i = 0; i <= 300; i+= this.cellHeight) { //horizontal lines
+   this.ctx.moveTo(0, i);
+   this.ctx.lineTo(300, i);
  }
 
- Gameboard.prototype.drawBoard = function () {
-   this.ctx.beginPath();
+ for (var i = 0; i <= 300; i+= this.cellWidth) { //vertical lines
+   this.ctx.moveTo(i, 0);
+   this.ctx.lineTo(i, 300);
+ }
 
-   for (var i = 0; i <= 300; i+= this.cellHeight) { //horizontal lines
-     this.ctx.moveTo(0, i);
-     this.ctx.lineTo(300, i);
-   }
+ this.ctx.stroke();
+};
 
-   for (var i = 0; i <= 300; i+= this.cellWidth) { //vertical lines
-     this.ctx.moveTo(i, 0);
-     this.ctx.lineTo(i, 300);
-   }
+Gameboard.prototype.drawPlayer = function(row, col, playerId) {
+  var radius = (this.cellWidth / 2) - 1,
+      x      = (this.cellWidth * col) - (this.cellWidth/2),
+      y      = (this.cellHeight * row) - (this.cellHeight/2);
 
-   this.ctx.stroke();
- };
+  //temporary until I figure out a better way to handle user's avatars
+  var playerImage = document.getElementById('userAvatar');
+
+  console.log(playerImage);
+
+  this.ctx.beginPath();
+  this.ctx.moveTo(x, y);
+  this.ctx.arc(x, y, radius, 0, Math.PI * 2);
+  this.ctx.stroke();
+  this.ctx.clip();
+  this.ctx.drawImage(playerImage, x - (this.cellWidth/2), y - (this.cellHeight/2), this.cellWidth, this.cellHeight);
+
+ }
